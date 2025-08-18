6ffd6db68df909efb4954f8b860fc094bb800a7f
ParkHyeonBeom <gusqja0213@naver.com>
2025-08-18 22:12:40 +0900

    Feat: Topic 모델 생성

--- core/models.py
+++ core/models.py
@@ -58,3 +58,26 @@ class Newsletter(models.Model):
 
     def __str__(self):
         return f"[{self.user.email}] {self.title}"
+
+
+class Topic(models.Model):
+    """
+    사용자가 구독할 관심 주제 모델
+    """
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="주제명"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="생성일"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "관심 주제"
+        verbose_name_plural = f"{verbose_name} 목록"
+        ordering = ['-created_at']
